b367bb25401add430c40b8e251bbaf2534c9147d
Ravi Kumar <gravi625@gmail.com>
2023-02-28 09:58:03 +0530

    Add: day2 practice problem is added

---
+++ Day2/temp/helloworld.java
@@ -0,0 +1,11 @@
+public class Helloworld{
+   public static void main(String[] args){
+        while(true) {
+       System.out.println("hello world!");
+         try {
+          Thread.sleep(6000);
+           } catch (Exception ex){}
+
+     }
+   }
+}
